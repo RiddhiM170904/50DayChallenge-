38cf040cfe741d545f5fa6e0ef9cb9cf8a89ec7a
RiddhiM170904 <riddhimhadgut17@gmail.com>
2025-05-09 21:01:59 +0530

    day 10 added

---
+++ day-10/Day.10.1.cpp
@@ -0,0 +1,8 @@
+class Solution {
+    public:
+        int bulbSwitch(int n) {
+            return sqrt(n);
+        }
+    };
+
+// The above code defines a class Solution with a public method bulbSwitch that takes an integer n as input and returns the square root of n.
