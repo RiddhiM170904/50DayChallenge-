21a958642c40aff0ac3e71e28eca213898107893
Riddhi Mhadgut <riddhimhadgut17@gmail.com>
2025-06-12 22:28:43 +0530

    Create day.44.2.cpp

---
+++ day-44/day.44.2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        string vowels="aeiou";
+        int n=s.length();
+        int count=0;
+        int curr=0;
+        for(int i=0;i<k;i++){
+            if(vowels.find(s[i])!=string::npos){
+                curr++;
+            }
+        }
+        count=curr;
+        for(int i=k;i<n;i++){
+            if(vowels.find(s[i])!=string::npos){
+                curr++;
+            }
+            if(vowels.find(s[i-k])!=string::npos){
+                curr--;
+            }
+
+            count=max(count,curr);
+        }
+
+        return count;
+    }
+};
